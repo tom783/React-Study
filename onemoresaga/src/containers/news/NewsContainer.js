609aefd3c445ba147da8ac702ece02ae806be3b2
tom783 <dlgudwns783@gmail.com>
2020-03-06 12:32:39 +0900

    onemore newscontainer apply witRouter

--- onemoresaga/src/containers/news/NewsContainer.js
+++ onemoresaga/src/containers/news/NewsContainer.js
@@ -1,12 +1,115 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {withRouter} from 'react-router-dom'
 import {PagingContainer} from 'containers/paging'
+import styled from 'styled-components';
 
-const NewsContainer = () => {
+
+const NewsContainer = props => {
+
+  const [value, setValue] = useState({
+    tab: "1",
+    lenderFirst: true,
+  });
+
+  const changeTab = e => {
+    console.log("CLICK", e.target.dataset.tab);
+    const targetTab = e.target.dataset.tab;
+    
+    setValue({
+      tab: targetTab
+    });
+  }
+  
+  useEffect(() => {
+    if(value.lenderFirst){
+      setValue({
+        lenderFirst: false
+      })
+    }
+  },[]);
+
+  useEffect(() => {
+    if(!value.lenderFirst){
+      props.history.push(`/news/${value.tab}`);
+    }
+  }, [value.tab, value.lenderFirst]);
+
+  useEffect(() => {
+    console.log("url path", props.location.pathname);
+    const path = props.location.pathname;
+    if(path === '/news/1'){
+      setValue({
+        tab: "1"
+      })
+    }else if(path === '/news/2'){
+      setValue({
+        tab: "2"
+      })
+    }
+  }, [props.location.pathname]);
+
+  
+const component1 = (
+  <div>
+      components1
+  </div>
+)
+
+
+const component2 = (
+  <div>
+    components2
+  </div>
+)
+
   return (
-    <div>
-      <PagingContainer />
-    </div>
+    <Wrap>
+      {/* <PagingContainer /> */}
+      <TestNav>
+        <h3>testNave</h3>
+        <div className="link" data-tab="1" onClick={changeTab}>
+          nav1
+        </div>
+        <div className="link" data-tab="2" onClick={changeTab}>
+          nav2
+        </div>
+      </TestNav>
+
+      <Cont>
+        {
+          value.tab === "1"?
+          component1
+          :
+          (
+            value.tab === "2"?
+            component2
+            :
+            null
+          )
+        }
+      </Cont>
+    </Wrap>
   );
 };
 
-export default NewsContainer;
+
+const Wrap = styled.div`
+  display: grid;
+  grid-template-columns: 25% auto;
+`
+
+const TestNav = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-column: 1;
+  
+  .link{
+    cursor: pointer;
+  }
+`
+
+const Cont = styled.div`
+  grid-column: 2;
+`
+
+export default withRouter(NewsContainer);
